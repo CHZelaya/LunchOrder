561d8145348b90a6842eac72080855067ce50714
CHZelaya <chzelaya1201@gmail.com>
2024-10-24 21:05:23 -0600

    Form designed, initial constants declared.

--- LunchOrder/LunchOrder.Designer.cs
+++ LunchOrder/LunchOrder.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace LunchOrder
+﻿
+
+
+namespace LunchOrder
 {
     partial class FRMLunchOrder
     {
@@ -28,18 +31,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            GroupBoxMain = new GroupBox();
+            RadioSalad = new RadioButton();
+            RadioPizza = new RadioButton();
+            RadioHamburger = new RadioButton();
+            GroupBoxAddOn = new GroupBox();
+            CheckAddOn3 = new CheckBox();
+            CheckAddOn2 = new CheckBox();
+            CheckAddOn1 = new CheckBox();
+            GroupBoxTotal = new GroupBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            ButtonPlaceOrder = new Button();
+            ButtonExitForm = new Button();
+            ButtonResetForm = new Button();
+            GroupBoxMain.SuspendLayout();
+            GroupBoxAddOn.SuspendLayout();
+            GroupBoxTotal.SuspendLayout();
             SuspendLayout();
+            // 
+            // GroupBoxMain
+            // 
+            GroupBoxMain.BackColor = SystemColors.WindowFrame;
+            GroupBoxMain.Controls.Add(RadioSalad);
+            GroupBoxMain.Controls.Add(RadioPizza);
+            GroupBoxMain.Controls.Add(RadioHamburger);
+            GroupBoxMain.ForeColor = SystemColors.ButtonFace;
+            GroupBoxMain.Location = new Point(31, 60);
+            GroupBoxMain.Name = "GroupBoxMain";
+            GroupBoxMain.Size = new Size(185, 115);
+            GroupBoxMain.TabIndex = 0;
+            GroupBoxMain.TabStop = false;
+            GroupBoxMain.Text = "Main Course";
+            // 
+            // RadioSalad
+            // 
+            RadioSalad.AutoSize = true;
+            RadioSalad.Location = new Point(17, 75);
+            RadioSalad.Name = "RadioSalad";
+            RadioSalad.Size = new Size(53, 19);
+            RadioSalad.TabIndex = 2;
+            RadioSalad.TabStop = true;
+            RadioSalad.Text = "Salad";
+            RadioSalad.UseVisualStyleBackColor = true;
+            // 
+            // RadioPizza
+            // 
+            RadioPizza.AutoSize = true;
+            RadioPizza.Location = new Point(17, 50);
+            RadioPizza.Name = "RadioPizza";
+            RadioPizza.Size = new Size(51, 19);
+            RadioPizza.TabIndex = 1;
+            RadioPizza.TabStop = true;
+            RadioPizza.Text = "Pizza";
+            RadioPizza.UseVisualStyleBackColor = true;
+            // 
+            // RadioHamburger
+            // 
+            RadioHamburger.AutoSize = true;
+            RadioHamburger.Location = new Point(17, 25);
+            RadioHamburger.Name = "RadioHamburger";
+            RadioHamburger.Size = new Size(86, 19);
+            RadioHamburger.TabIndex = 0;
+            RadioHamburger.TabStop = true;
+            RadioHamburger.Text = "Hamburger";
+            RadioHamburger.UseVisualStyleBackColor = true;
+            // 
+            // GroupBoxAddOn
+            // 
+            GroupBoxAddOn.Controls.Add(CheckAddOn3);
+            GroupBoxAddOn.Controls.Add(CheckAddOn2);
+            GroupBoxAddOn.Controls.Add(CheckAddOn1);
+            GroupBoxAddOn.Location = new Point(282, 60);
+            GroupBoxAddOn.Name = "GroupBoxAddOn";
+            GroupBoxAddOn.Size = new Size(206, 115);
+            GroupBoxAddOn.TabIndex = 1;
+            GroupBoxAddOn.TabStop = false;
+            GroupBoxAddOn.Text = "Add-On Items";
+            // 
+            // CheckAddOn3
+            // 
+            CheckAddOn3.AutoSize = true;
+            CheckAddOn3.Location = new Point(17, 78);
+            CheckAddOn3.Name = "CheckAddOn3";
+            CheckAddOn3.Size = new Size(83, 19);
+            CheckAddOn3.TabIndex = 2;
+            CheckAddOn3.Text = "checkBox3";
+            CheckAddOn3.UseVisualStyleBackColor = true;
+            // 
+            // CheckAddOn2
+            // 
+            CheckAddOn2.AutoSize = true;
+            CheckAddOn2.Location = new Point(17, 53);
+            CheckAddOn2.Name = "CheckAddOn2";
+            CheckAddOn2.Size = new Size(83, 19);
+            CheckAddOn2.TabIndex = 1;
+            CheckAddOn2.Text = "checkBox2";
+            CheckAddOn2.UseVisualStyleBackColor = true;
+            // 
+            // CheckAddOn1
+            // 
+            CheckAddOn1.AutoSize = true;
+            CheckAddOn1.Location = new Point(17, 28);
+            CheckAddOn1.Name = "CheckAddOn1";
+            CheckAddOn1.Size = new Size(83, 19);
+            CheckAddOn1.TabIndex = 0;
+            CheckAddOn1.Text = "checkBox1";
+            CheckAddOn1.UseVisualStyleBackColor = true;
+            // 
+            // GroupBoxTotal
+            // 
+            GroupBoxTotal.Controls.Add(label3);
+            GroupBoxTotal.Controls.Add(label2);
+            GroupBoxTotal.Controls.Add(label1);
+            GroupBoxTotal.Controls.Add(textBox3);
+            GroupBoxTotal.Controls.Add(textBox2);
+            GroupBoxTotal.Controls.Add(textBox1);
+            GroupBoxTotal.Location = new Point(31, 229);
+            GroupBoxTotal.Name = "GroupBoxTotal";
+            GroupBoxTotal.Size = new Size(246, 127);
+            GroupBoxTotal.TabIndex = 2;
+            GroupBoxTotal.TabStop = false;
+            GroupBoxTotal.Text = "Order Total";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(17, 88);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Order Total:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(17, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Tax (5%):";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(17, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Subtotal:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(102, 80);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(102, 51);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(102, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // ButtonPlaceOrder
+            // 
+            ButtonPlaceOrder.Location = new Point(352, 229);
+            ButtonPlaceOrder.Name = "ButtonPlaceOrder";
+            ButtonPlaceOrder.Size = new Size(75, 23);
+            ButtonPlaceOrder.TabIndex = 3;
+            ButtonPlaceOrder.Text = "Place Order";
+            ButtonPlaceOrder.UseVisualStyleBackColor = true;
+            ButtonPlaceOrder.Click += this.ButtonPlaceOrder_Click;
+            // 
+            // ButtonExitForm
+            // 
+            ButtonExitForm.Location = new Point(352, 333);
+            ButtonExitForm.Name = "ButtonExitForm";
+            ButtonExitForm.Size = new Size(75, 23);
+            ButtonExitForm.TabIndex = 4;
+            ButtonExitForm.Text = "Exit";
+            ButtonExitForm.UseVisualStyleBackColor = true;
+            // 
+            // ButtonResetForm
+            // 
+            ButtonResetForm.Location = new Point(352, 279);
+            ButtonResetForm.Name = "ButtonResetForm";
+            ButtonResetForm.Size = new Size(75, 23);
+            ButtonResetForm.TabIndex = 5;
+            ButtonResetForm.Text = "Reset";
+            ButtonResetForm.UseVisualStyleBackColor = true;
             // 
             // FRMLunchOrder
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(514, 386);
+            Controls.Add(ButtonResetForm);
+            Controls.Add(ButtonExitForm);
+            Controls.Add(ButtonPlaceOrder);
+            Controls.Add(GroupBoxTotal);
+            Controls.Add(GroupBoxAddOn);
+            Controls.Add(GroupBoxMain);
             Name = "FRMLunchOrder";
             Text = "Lunch Order";
+            GroupBoxMain.ResumeLayout(false);
+            GroupBoxMain.PerformLayout();
+            GroupBoxAddOn.ResumeLayout(false);
+            GroupBoxAddOn.PerformLayout();
+            GroupBoxTotal.ResumeLayout(false);
+            GroupBoxTotal.PerformLayout();
             ResumeLayout(false);
         }
 
+      
+
+
+
         #endregion
+
+        private GroupBox GroupBoxMain;
+        private RadioButton RadioSalad;
+        private RadioButton RadioPizza;
+        private RadioButton RadioHamburger;
+        private GroupBox GroupBoxAddOn;
+        private CheckBox CheckAddOn1;
+        private CheckBox CheckAddOn3;
+        private CheckBox CheckAddOn2;
+        private GroupBox GroupBoxTotal;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button ButtonPlaceOrder;
+        private Button ButtonExitForm;
+        private Button ButtonResetForm;
     }
 }
--- LunchOrder/LunchOrder.cs
+++ LunchOrder/LunchOrder.cs
@@ -2,9 +2,21 @@ namespace LunchOrder
 {
     public partial class FRMLunchOrder : Form
     {
+        const double HAMBURGER_PRICE = 6.95;
+        const double PIZZA = 5.95;
+        const double SALAD = 4.95;
+        const double ADD_ON = 0.25;
         public FRMLunchOrder()
         {
             InitializeComponent();
         }
+        private void ButtonPlaceOrder_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
     }
+
 }
